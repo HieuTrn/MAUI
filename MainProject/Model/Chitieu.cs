269b283500c23eff255d8749e6d90a8b9e0ef9f8
SiesShin162 <Shinosenisu@gmail.com>
2026-04-05 21:47:54 +0700

    bỏ bảng vi

--- MainProject/Model/Chitieu.cs
+++ MainProject/Model/Chitieu.cs
@@ -9,15 +9,8 @@ namespace MainProject.Models
         public int Id { get; set; }
         public string user { get; set; }
       public string password { get; set; }
-
-    }
-    public class vi
-    {
-
-        public int Id { get; set; }
-        public string ten { get; set; }
         public double sotien { get; set; }
-        public int IDtk { get; set; }
+
     }
 
     public class giaodich
@@ -26,7 +19,7 @@ namespace MainProject.Models
         public int Id { get; set; }
         public double sotien { get; set; }
         public string LoaiGD { get; set; } 
-        public int IDvi { get; set; }
+        public int IDtk { get; set; }
         public int IDdanhmuc { get; set; }
         public DateTime ngay { get; set; }
         public string ghichu { get; set; }
