1ac3627035b550282d31528c49e558a20568dcc4
Trung Anh <2355010014@kientruchanoi.edu.vn>
2026-04-05 23:32:09 +0700

    tương tác chuột

--- MainProject/Pages/GiaoDichPage.xaml.cs
+++ MainProject/Pages/GiaoDichPage.xaml.cs
@@ -6,6 +6,37 @@ public partial class GiaoDichPage : ContentPage
     {
         InitializeComponent();
     }
+    private void OnTabClicked(object sender, TappedEventArgs e)
+    {
+        // Reset tất cả tab về mặc định
+        var tabs = new[] { TabTatCa, TabThuNhap, TabChiTieu, TabChuyenKhoan };
+        var lbls = new[] { LblTatCa, LblThuNhap, LblChiTieu, LblChuyenKhoan };
+
+        foreach (var tab in tabs) tab.BackgroundColor = Colors.Transparent;
+        foreach (var lbl in lbls) lbl.TextColor = Color.FromArgb("#64748B");
+
+        // Bật tab được chọn
+        string param = e.Parameter?.ToString();
+        switch (param)
+        {
+            case "TatCa":
+                TabTatCa.BackgroundColor = Color.FromArgb("#193CB8");
+                LblTatCa.TextColor = Colors.White;
+                break;
+            case "ThuNhap":
+                TabThuNhap.BackgroundColor = Color.FromArgb("#193CB8");
+                LblThuNhap.TextColor = Colors.White;
+                break;
+            case "ChiTieu":
+                TabChiTieu.BackgroundColor = Color.FromArgb("#193CB8");
+                LblChiTieu.TextColor = Colors.White;
+                break;
+            case "ChuyenKhoan":
+                TabChuyenKhoan.BackgroundColor = Color.FromArgb("#193CB8");
+                LblChuyenKhoan.TextColor = Colors.White;
+                break;
+        }
+    }
 
     // Nút FAB "+" → điều hướng sang trang Thêm Giao Dịch
     private async void OnThemGiaoDichClicked(object sender, EventArgs e)
@@ -13,3 +44,5 @@ public partial class GiaoDichPage : ContentPage
         await Navigation.PushModalAsync(new ThemGiaoDichPage());
     }
 }
+
+
